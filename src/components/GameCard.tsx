ab94db8c25d9d78670a8fbfbcbf2788e6bd1e311
Amna <dhrioua.amna@gmail.com>
2023-08-18 11:29:09 +0100

    Refactor: remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Game } from '../hooks2/useGames';
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList'; // You need to import PlatformIconList if it's not imported already
-
+import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image.url';
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image} />
+    <Card >
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
         {/* Assuming PlatformIconList is a properly defined component */}
-        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+        <HStack justifyContent='space-between'>
+          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+          <CriticScore score = {game.metacritic}/>
+        </HStack>
+        
       </CardBody>
     </Card>
   );
